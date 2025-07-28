eea2e1f09cee4a65d0dc01096ca96497b22fef35
chsong <chsong1@cloit.com>
2025-07-28 14:49:40 +0900

    f

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -25,24 +25,4 @@
 
 // 트랙패드 방향 설정
 #define AZOTEQ_IQS5XX_ROTATION_90
-// 추가 포인팅 디바이스 설정 
-// #define POINTING_DEVICE_ROTATION_270
-// #define POINTING_DEVICE_INVERT_X
-// #define POINTING_DEVICE_INVERT_Y
 
-// 디버그 활성화 (필요시 활성화)
-// #define DEBUG_MATRIX_SCAN_RATE   
-// #define DEBUG_ENABLE
-// #define POINTING_DEVICE_DEBUG
-
-// 하드웨어 디바운싱 설정
-#define DEBOUNCE 5
-
-// Vial 설정
-#include "vial.h"
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 1 }
-#define VIAL_TAP_DANCE_ENTRIES 4
-
-/* RP2040-specific Vial settings */
-#define VIAL_EEPROM_LAYOUT_OPTIONS_SIZE 1
